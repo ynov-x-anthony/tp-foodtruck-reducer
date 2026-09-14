5f8b25a93e37a52a8b99a03e6541a406f10622fd
basttouuu <hug0.paulier83@gmail.com>
2026-09-14 16:33:06 +0200

    Todo 3 Fini

--- starter/src/context/CartContext.tsx
+++ starter/src/context/CartContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, type Dispatch, type ReactNode } from "react";
+import { createContext, useContext, useReducer, type Dispatch, type ReactNode } from "react";
 import { cartReducer, initialCartState } from "../reducer/cartReducer";
 import type { CartAction, CartState } from "../types";
 
@@ -26,10 +26,12 @@ export function CartProvider({ children }: { children: ReactNode }) {
   // TODO 3.1 : appelle useReducer avec `cartReducer` et `initialCartState`
   // pour obtenir `state` et `dispatch`. (Import à ajouter : `useReducer`
   // depuis "react".)
+  const [state, dispatch] = useReducer(cartReducer,initialCartState);
+
 
   // TODO 3.2 : remplace le fragment ci-dessous par
   // <CartContext.Provider value={{ state, dispatch }}>{children}</CartContext.Provider>
-  return <>{children}</>; // 🔧 à remplacer
+  return <CartContext.Provider value={{ state, dispatch }}>{children}</CartContext.Provider>; // 🔧 à remplacer (c'est remplacer)
 }
 
 /**
